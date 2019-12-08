5f64d91064a0dbb700c24853dc55da366c3ec6fd
Abtaha <abtaha2005@gmail.com>
2019-12-08 20:27:26 +0500

    Added Comments

--- scanner.py
+++ scanner.py
@@ -1,10 +1,15 @@
 import socket
 
+# Takes address as input
 addr = input("Enter IP address >>> ")
 
-for port in range(0, 65535):
+
+# Loops over all the ports
+for port in range(65535 + 1):
+    # Creates socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, )
     
+    # Connects to socket
     result = sock.connect_ex((addr, port))
     if result == 0:
         print("Port ", port, " is open")
